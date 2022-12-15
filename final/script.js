97f100939342405df3f4509c859f86f8f6037927
Artisme <darrenonly@outlook.com>
2022-12-15 22:16:20 +0800

    Remove unused CSS and do some clean up

---
+++ final/script.js
@@ -0,0 +1,14 @@
+// Sidebar
+document.querySelector(".navbar-toggle").addEventListener("click", () => {
+  document.querySelector("#main-collapse").classList.toggle("open");
+  document.querySelector(".sidebar").classList.toggle("open");
+});
+// Active page
+let x = document.querySelector('nav li a[href=".' + location.pathname + '"]');
+if (x) {
+  x.classList.add("active");
+} else {
+  document
+    .querySelector('nav li a[href="./index.html"]')
+    .classList.add("active");
+}
